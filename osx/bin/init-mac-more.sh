7aeaa339b0f6d8e90ba9eaf8b6b39de93fa2500c
Matt Light <matt.light@lightdatasys.com>
2014-10-20 10:00:00 -0700

    Disable OSX dashboard

--- osx/bin/init-mac-more.sh
+++ osx/bin/init-mac-more.sh
@@ -45,6 +45,12 @@ defaults write com.apple.dock show-process-indicators -bool true
 # (i.e. use the old Exposé behavior instead)
 defaults write com.apple.dock expose-group-by-app -bool false
 
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
