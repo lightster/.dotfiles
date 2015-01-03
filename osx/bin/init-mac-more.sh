3e212c79ac245a7d757cf82d1ae31d5c80cf74c6
Matt Light <matt.light@lightdatasys.com>
2015-01-03 11:47:50 -0800

    Set OSX highlight color to apple green

--- osx/bin/init-mac-more.sh
+++ osx/bin/init-mac-more.sh
@@ -9,6 +9,14 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until this script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
+
+# Set text highlight color to beautiful light/apple green
+defaults write NSGlobalDomain AppleHighlightColor -string "0.752941 0.964706 0.678431"
+
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
