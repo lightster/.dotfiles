8ca85049d0bfc08dc84120afe1981fde401af928
Matt Light <matt.light@lightdatasys.com>
2015-01-08 17:52:09 -0800

    Add Dash to Mac setup instructions

--- osx/bin/init-mac.sh
+++ osx/bin/init-mac.sh
@@ -33,6 +33,7 @@ echo '  sudo mv composer.phar /usr/local/bin/composer'
 echo 'Install phpcs (composer global require "squizlabs/php_codesniffer=*")'
 echo 'Install php-cs-fixer (composer global require fabpot/php-cs-fixer @stable)'
 echo 'Install Alfred and setup sync to Dropbox'
+echo 'Install Dash'
 echo 'Install Caffeine'
 
 echo "Validating for sudo... "
