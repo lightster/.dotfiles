fe0d5bf1225fe596cc80e96606ffaefbeff9a2e6
Matt Light <matt.light@illuminateed.com>
2016-07-02 18:12:53 -0700

    Make 'make-done' re-runnable

--- osx/bin/make-done.sh
+++ osx/bin/make-done.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 HOSTNAME=$(hostname -s)
 
 if [ ! -f "${HOME}/.ssh/id_rsa.${HOSTNAME}" ]; then
@@ -11,27 +9,33 @@ fi
 
 echo "Switch .dotfiles from https:// to git://"
 cd ~/.dotfiles
-git remote add origin git://github.com/lightster/.dotfiles.git
-git fetch origin
-git branch -u origin/master
+git remote show origin
+if [ "$?" != "0" ]; then
+    git remote add origin git://github.com/lightster/.dotfiles.git
+    git fetch origin
+    git branch -u origin/master
+fi
 cd - >/dev/null
 
 echo "Switch .ssh from https:// to git://"
 cd ~/.ssh
-git remote add origin git://github.com/lightster/.ssh.git
-git fetch origin
-git branch -u origin/master
+git remote show origin
+if [ "$?" != "0" ]; then
+    git remote add origin git://github.com/lightster/.ssh.git
+    git fetch origin
+    git branch -u origin/master
+fi
 cd - >/dev/null
 
 echo ""
 echo "Killing ssh-agent"
-set +e
 sudo killall ssh-agent
-set -e
 
 echo ""
 echo "Cloning the .ssh-private repo to ~/.ssh/private..."
-git clone git@github.com:lightster/.ssh-private.git ~/.ssh/private
-cd ~/.ssh
-./bin/sshk-update
-cd - >/dev/null
+if [ ! -d ~/.ssh/private ]; then
+    git clone git@github.com:lightster/.ssh-private.git ~/.ssh/private
+    cd ~/.ssh
+    ./bin/sshk-update
+    cd - >/dev/null
+fi
