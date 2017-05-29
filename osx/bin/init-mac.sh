ba8ae1849e330b41fcd8cc70b5f38d85553b628f
Matt Light <matt.light@lightdatasys.com>
2017-05-29 00:05:21 -0700

    Fix various issues with RVM installation

--- osx/bin/init-mac.sh
+++ osx/bin/init-mac.sh
@@ -72,8 +72,8 @@ brew cask install vagrant
 brew cask install virtualbox
 brew cask install qlmarkdown
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable -- --ignore-dotfiles
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles stable
 
 sudo gem install travis -v 1.8.2 --no-rdoc --no-ri
 
