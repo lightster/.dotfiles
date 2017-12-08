10be29a47b82f9e9a762bc1647bf397bea800aaf
Matt Light <matt.light@lightdatasys.com>
2017-12-07 22:40:50 -0800

    Add `jq` to brew dependencies
    For formatting JSON on the CLI!

--- osx/bin/init-mac.sh
+++ osx/bin/init-mac.sh
@@ -37,6 +37,7 @@ brew tap caskroom/versions
 brew install ack
 brew install ansible
 brew install dockutil
+brew install jq
 brew install git
 brew install --with-default-names gnu-sed
 brew install gpg
