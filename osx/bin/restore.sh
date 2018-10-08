1a9288e8861092ab3874fe441e63469c766ef11a
Matt Light <matt@paren.io>
2018-10-07 23:36:35 -0700

    Update snapshot/restore scripts for my current dev workflow

--- osx/bin/restore.sh
+++ osx/bin/restore.sh
@@ -6,15 +6,22 @@ if [ "$#" -ne 1 ]; then
 fi
 
 export BACKUP_NAME="${1}"
+export BACKUP_VOLUME=Pollywog
 
 COMMANDS=$(cat <<COMMANDS
-rsync -aP /Volumes/lightster-homedir/${BACKUP_NAME}/*.sparsebundle ~/Disks/
-rsync -aP /Volumes/lightster-homedir/${BACKUP_NAME}/Messages/ ~/Library/Messages/
-rsync -aP /Volumes/lightster-homedir/${BACKUP_NAME}/hidden.ssh/ ~/.ssh/
-rsync -aP /Volumes/lightster-homedir/${BACKUP_NAME}/Application\ Support/ ~/Library/Application\ Support/
-rsync -aP /Volumes/lightster-homedir/${BACKUP_NAME}/hidden.aws/ ~/.aws/
-rsync -aP /Volumes/lightster-homedir/${BACKUP_NAME}/hidden.gnupg/ ~/.gnupg/
-rsync -aP /Volumes/lightster-homedir/${BACKUP_NAME}/history/.*history* ~/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/*.sparsebundle ~/Disks/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/hidden.dotfiles/ ~/.dotfiles/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/backup/ ~/backup/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/github/ ~/github/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/.github.bk/ ~/github.bk/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/go/ ~/go/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/Messages/ ~/Library/Messages/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/hidden.ssh/ ~/.ssh/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/tmux/ ~/.tmux/
+rsync -a --partial '"'/Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/Application\ Support/'"' '"'${HOME}/Library/Application\ Support/'"'
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/hidden.aws/ ~/.aws/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/hidden.gnupg/ ~/.gnupg/
+rsync -a --partial /Volumes/${BACKUP_VOLUME}/${BACKUP_NAME}/history/.*history* ~/
 COMMANDS
 )
 
