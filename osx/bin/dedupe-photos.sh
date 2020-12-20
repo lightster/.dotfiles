a7f0706f9e61befc84d25afc4b11d25b475a5e70
Matt Light <matt@paren.io>
2020-12-19 22:03:44 -0800

    Commit previous changes to dedupe-photos

--- osx/bin/dedupe-photos.sh
+++ osx/bin/dedupe-photos.sh
@@ -4,6 +4,17 @@ set -e
 
 cd ~/Dropbox/Pictures
 
+# Organize movies thats have CreationDate set
+find -E Unprocessed \
+  -type f \
+  -iregex '.*\.(mov)' \
+  -print0 | xargs -0 -I {} \
+    exiftool \
+      -P -d '%Y/%m/%Y%m%d_%H%M%S' \
+        '-filename<Organized/Devices/Unknown/${CreationDate}_000%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${CreationDate}_000%-c.%le' \
+        {}
+
 # organize Unprocessed photos by date into Organized
 find -E Unprocessed \
   -type f \
@@ -11,26 +22,26 @@ find -E Unprocessed \
   -print0 | xargs -0 -I {} \
     exiftool \
       -P -d '%Y/%m/%Y%m%d_%H%M%S' \
-        '-filename<Organized/Unknown/${FileModifyDate}_000%-c.%le' \
-        '-filename<Organized/Unknown/${GPSDateTime}_000%-c.%le' \
-        '-filename<Organized/Unknown/${MediaCreateDate}_000%-c.%le' \
-        '-filename<Organized/Unknown/${ModifyDate}_000%-c.%le' \
-        '-filename<Organized/Unknown/${DateTimeOriginal}_000%-c.%le' \
-        '-filename<Organized/Unknown/${FileModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/Unknown/${GPSDateTime}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/Unknown/${MediaCreateDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/Unknown/${ModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/Unknown/${DateTimeOriginal}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/${Model;}/${FileModifyDate}_000%-c.%le' \
-        '-filename<Organized/${Model;}/${GPSDateTime}_000%-c.%le' \
-        '-filename<Organized/${Model;}/${MediaCreateDate}_000%-c.%le' \
-        '-filename<Organized/${Model;}/${ModifyDate}_000%-c.%le' \
-        '-filename<Organized/${Model;}/${DateTimeOriginal}_000%-c.%le' \
-        '-filename<Organized/${Model;}/${FileModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/${Model;}/${GPSDateTime}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/${Model;}/${MediaCreateDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/${Model;}/${ModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/${Model;}/${DateTimeOriginal}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${FileModifyDate}_000%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${GPSDateTime}_000%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${MediaCreateDate}_000%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${ModifyDate}_000%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${DateTimeOriginal}_000%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${FileModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${GPSDateTime}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${MediaCreateDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${ModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/Unknown/${DateTimeOriginal}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${FileModifyDate}_000%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${GPSDateTime}_000%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${MediaCreateDate}_000%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${ModifyDate}_000%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${DateTimeOriginal}_000%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${FileModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${GPSDateTime}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${MediaCreateDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${ModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/${Model;}/${DateTimeOriginal}_${SubSecTimeOriginal;}%-c.%le' \
         {}
 
 # organize Unprocessed gifs/pngs by date into Organized/${lowercase-extension}
@@ -40,16 +51,16 @@ find -E Unprocessed \
   -print0 | xargs -0 -I {} \
     exiftool \
       -P -d '%Y/%Y%m%d_%H%M%S' \
-        '-filename<Organized/%le/${FileModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/%le/${GPSDateTime}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/%le/${MediaCreateDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/%le/${ModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/%le/${DateTimeOriginal}_${SubSecTimeOriginal;}%-c.%le' \
-        '-filename<Organized/%le/${FileModifyDate}_000%-c.%le' \
-        '-filename<Organized/%le/${GPSDateTime}_000%-c.%le' \
-        '-filename<Organized/%le/${MediaCreateDate}_000%-c.%le' \
-        '-filename<Organized/%le/${ModifyDate}_000%-c.%le' \
-        '-filename<Organized/%le/${DateTimeOriginal}_000%-c.%le' \
+        '-filename<Organized/Devices/%le/${FileModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/%le/${GPSDateTime}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/%le/${MediaCreateDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/%le/${ModifyDate}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/%le/${DateTimeOriginal}_${SubSecTimeOriginal;}%-c.%le' \
+        '-filename<Organized/Devices/%le/${FileModifyDate}_000%-c.%le' \
+        '-filename<Organized/Devices/%le/${GPSDateTime}_000%-c.%le' \
+        '-filename<Organized/Devices/%le/${MediaCreateDate}_000%-c.%le' \
+        '-filename<Organized/Devices/%le/${ModifyDate}_000%-c.%le' \
+        '-filename<Organized/Devices/%le/${DateTimeOriginal}_000%-c.%le' \
         {}
 
 # remove Organized files that have duplicate content
