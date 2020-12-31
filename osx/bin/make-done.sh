5c5eca99d215e5b50c8019f3dfdfaf0e2889f519
Matt Light <matt@paren.io>
2020-12-30 22:41:34 -0800

    Improve status output of `make done`

--- osx/bin/make-done.sh
+++ osx/bin/make-done.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 HOSTNAME=$(hostname -s)
 
@@ -12,23 +12,30 @@ echo "Killing ssh-agent"
 sudo killall ssh-agent
 
 echo ""
-echo "Cloning the .ssh-private repo to ~/.ssh/private..."
 if [ ! -d ~/.ssh/private ]; then
+    echo "cloning .ssh-private"
     export GIT_SSH_COMMAND="ssh -i ${HOME}/.ssh/id_rsa.${HOSTNAME} -F /dev/null"
     git clone git@github.com:lightster/.ssh-private.git ~/.ssh/private
     cd ~/.ssh
     ./bin/sshk-update
     cd - >/dev/null
+    echo "done cloning .ssh-private"
+else
+    echo ".ssh-private already cloned"
 fi
 
 if [ -d ~/.ssh ]; then
+    echo -n "updating .ssh remote origin to use ssh protocol... "
     cd ~/.ssh
     git remote set-url origin git@github.com:lightster/.ssh.git
     cd - >/dev/null
+    echo "done"
 fi
 
 if [ -d ~/.dotfiles ]; then
+    echo -n "updating .dotfiles remote origin to use ssh protocol... "
     cd ~/.dotfiles
     git remote set-url origin git@github.com:lightster/.dotfiles.git
     cd - >/dev/null
+    echo "done"
 fi
