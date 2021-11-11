7556bcc6d94360d61df9bbe4c8a2450e3880768c
Matt Light <matt@paren.io>
2021-11-11 15:54:56 -0800

    Make /usr/local/bin owned by me
    This ownership used to be set by Homebrew, but now Homebrew is installed
    in `/opt`

--- osx/bin/init-mac.sh
+++ osx/bin/init-mac.sh
@@ -16,6 +16,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until this script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+sudo chown $UID:$GID /usr/local/bin
+
 brew bundle --file ~/.dotfiles/osx/brew/core.brewfile
 if [ "$BUNDLE_NAME" != "" ]; then
   brew bundle --file ~/.dotfiles/osx/brew/"$BUNDLE_NAME".brewfile
