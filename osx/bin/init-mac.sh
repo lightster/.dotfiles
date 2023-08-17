300f8bf50c5f32332beaa24c9d3a06bca825becf
Matt Light <matt@paren.io>
2023-08-16 22:59:21 -0700

    Install node instead of nvm
    For now I will use pnpm to manage npm/node versions.

--- osx/bin/init-mac.sh
+++ osx/bin/init-mac.sh
@@ -42,20 +42,12 @@ git_clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git_clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git_clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git_clone https://github.com/zsh-users/zsh-completions.git ~/.zsh-completions
-git_clone https://github.com/nvm-sh/nvm.git ~/.nvm
 
 vim +PluginInstall +qall
 ~/.tmux/plugins/tpm/bin/install_plugins
 
 ~/.rvm/bin/rvm alias create default system
 
-cd ~/.nvm
-git checkout v0.37.2
-cd - >/dev/null
-
-export NVM_DIR="$HOME/.nvm"
-source ~/.dotfiles/shell/nvm
-nvm install 'lts/*' --latest-npm
 npm install -g npm http-server yarn
 
 sudo dscl . -create /Users/$USER UserShell /opt/homebrew/bin/zsh
